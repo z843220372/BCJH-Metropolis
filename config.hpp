af4b627d8d5830dca1fae22b93ad3fe733e9cb27
z843220372 <46099149+z843220372@users.noreply.github.com>
2023-06-02 16:08:05 +0800

    Update config.hpp
    11

--- config.hpp
+++ config.hpp
@@ -7,7 +7,7 @@
 #define NUM_CHEFS (NUM_GUESTS * CHEFS_PER_GUEST)
 
 #define MODE 1 // 1:宴会模式; 2:限时任务
-#define TARGET_SCORE_APPROXIMATE 2400000
+#define TARGET_SCORE_APPROXIMATE 595200
 // 预期达到的分数
 // 如果是限时任务，这里输分数线
 // 如果是宴会模式，不用特别准，差两倍/一半以内都可以
